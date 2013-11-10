3c6622121c77c788aa8ab8c8906437ee3b32be24
Tim Jarratt <tjarratt@gmail.com>
2013-11-10 12:35:10 -0800

    Move code around a bit
    Starting to think about how to test this, and what the
    interface will be. Nothing grand yet.

--- ext/librtlsdr/librtlsdr.c
+++ ext/librtlsdr/librtlsdr.c
@@ -1,13 +1,13 @@
 #include "ruby.h"
 
-extern VALUE m_turtleshell;
+VALUE m_turtleshell = Qnil;
 static VALUE m_rtlsdr;
 
 static void init_rtlsdr_module() {
-
 }
 
-void Init_lib_rtl_sdr_ext() {
+void Init_librtlsdr() {
+  m_turtleshell = rb_define_module("TurtleShell");
   m_rtlsdr = rb_define_module_under(m_turtleshell, "RTLSDR");
   rb_define_module_function(m_rtlsdr, "init", init_rtlsdr_module, 0);
 }
