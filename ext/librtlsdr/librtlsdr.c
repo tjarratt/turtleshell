26746fb9ced17c454ebb9a6e62d9284f76ec6d98
Tim Jarratt <tjarratt@gmail.com>
2013-12-21 17:36:37 -0800

    Fixup c extension
    Apparently methods defined on a module will always take
    self as the first argument, much as python handles instance
    methods.

--- ext/librtlsdr/librtlsdr.c
+++ ext/librtlsdr/librtlsdr.c
@@ -9,7 +9,7 @@ static VALUE turtleshell_count() {
   return INT2NUM(rtlsdr_get_device_count());
 }
 
-static VALUE turtleshell_new_device() {
+static VALUE turtleshell_first_device() {
   rtlsdr_dev_t *device = NULL;
   VALUE wrapped_device;
   VALUE hash;
@@ -39,11 +39,11 @@ static VALUE turtleshell_all_devices() {
   return devices_array;
 }
 
-static VALUE turtleshell_nth_device(VALUE n) {
+static VALUE turtleshell_nth_device(VALUE self, VALUE n) {
   uint32_t int_n = NUM2UINT(n);
   rtlsdr_dev_t *device = NULL;
-  VALUE wrapped_device;
-  VALUE hash;
+  VALUE hash, wrapped_device;
+
   if (n < rtlsdr_get_device_count()) { return Qnil; }
 
   rtlsdr_open(&device, int_n);
@@ -56,12 +56,32 @@ static VALUE turtleshell_nth_device(VALUE n) {
   return hash;
 }
 
+static VALUE turtleshell_read_synchronous(VALUE self,
+                                          VALUE device,
+                                          VALUE buffer,
+                                          VALUE bytes_to_read,
+                                          VALUE bytes_read_return_value) {
+
+}
+
+typedef void *(turtleshell_callback)(void *);
+static void turtleshell_read_asynchronous(VALUE self,
+                                          VALUE device,
+                                          VALUE buffer,
+                                          VALUE bytes_to_read,
+                                          VALUE bytes_read_return_value,
+                                          turtleshell_callback callback) {
+
+}
+
 void Init_librtlsdr() {
   m_turtleshell = rb_define_module("TurtleShell");
   m_rtlsdr = rb_define_module_under(m_turtleshell, "RTLSDR");
   c_device = rb_define_class_under(m_rtlsdr, "Device", rb_cObject);
   rb_define_module_function(m_rtlsdr, "count", turtleshell_count, 0);
-  rb_define_module_function(m_rtlsdr, "first_device", turtleshell_new_device, 0);
+  rb_define_module_function(m_rtlsdr, "first_device", turtleshell_first_device, 0);
   rb_define_module_function(m_rtlsdr, "all_devices", turtleshell_all_devices, 0);
-  rb_define_module_function(m_rtlsdr, "nth_device", turtleshell_nth_device, 1y);
+  rb_define_module_function(m_rtlsdr, "nth_device", turtleshell_nth_device, 1);
+  rb_define_module_function(m_rtlsdr, "read_sync", turtleshell_read_synchronous, 4);
+  rb_define_module_function(m_rtlsdr, "read_async", turtleshell_read_asynchronous, 5);
 }
