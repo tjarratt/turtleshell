9807fd24d028fa218357a5331f9e844449657901
Tim Jarratt <tjarratt@gmail.com>
2013-12-28 16:21:15 -0800

    Move examples into top level dir

--- examples/simple_read_from_device.rb
+++ examples/simple_read_from_device.rb
@@ -10,7 +10,7 @@ TurtleShell.all_devices.each_with_index do |device_name, index|
 end
 
 begin
-  sdr = TurtleShell::Device.new(0)
+  sdr = TurtleShell::Device.new
 rescue TurtleShell::DeviceNotFoundError
   puts 'No compatible devices found. bailing!'
   exit 1
